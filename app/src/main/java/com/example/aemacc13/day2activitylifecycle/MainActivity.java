ffd8b9dab57a08e40fc42a88165a62634a5d3f29
Alec MacCrory <aemacc13@stlawu.edu>
2016-09-01 11:55:50 -0400

    sep 1st

---
+++ app/src/main/java/com/example/aemacc13/day2activitylifecycle/MainActivity.java
@@ -0,0 +1,60 @@
+package com.example.aemacc13.day2activitylifecycle;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //if you need to do more view (UI) setup, do it here
+
+        TextView textView= (TextView) findViewById(R.id.hello_view);
+
+        //attach a click listener to the view
+        //use an anonymous inner class
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, R.string.toast_text, Toast.LENGTH_SHORT).show();
+
+                Intent intent= new Intent(MainActivity.this, Main2Activity.class);
+
+                intent.putExtra("PI", 3.14159);
+                startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+}
